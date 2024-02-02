d6405e8d6ed99e09920d595213e1d4d5162f699b
Timur <Stafr95@mail.ru>
2024-02-02 22:45:09 +0500

    Usage localStorage

--- src/js/index.js
+++ src/js/index.js
@@ -16,6 +16,13 @@ window.addEventListener('DOMContentLoaded', () => {
 	fetchDataBtn.addEventListener('click', fetchData);
 	clearTableBtn.addEventListener('click', clearTable);
 
+	// Использование localStorage загрузки данных при первой загрузке страницы
+	if (localStorage.getItem('tableData')) {
+		renderTable(JSON.parse(localStorage.getItem('tableData')) || []);
+		setupSortListener();
+		setupDeleteButtons();
+	}
+
 	function fetchData() {
 		// Показ прелоадера
 		showLoader();
@@ -32,6 +39,8 @@ window.addEventListener('DOMContentLoaded', () => {
 					renderTable(results);
 					setupSortListener();  // Обработчик событий для сортировки после загрузки данных
 					setupDeleteButtons(); // Обработчик событий для кнопки удаления
+					// Сохранение данных в localStorage
+					localStorage.setItem('tableData', JSON.stringify(results));
 				} else {
 					showPlaceholder('Данные не найдены');
 				}
@@ -46,26 +55,31 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	function renderTable(data) {
 		const table = document.createElement('table');
-		const headerRow = table.insertRow(0);
 
-		// Создание заголовков для таблицы
-		for (let j = 0; j < 10; j++) {
-			const th = document.createElement('th');
-			const key = Object.keys(data[0])[j];
-			th.textContent = key.charAt(0).toUpperCase() + key.slice(1);
-			headerRow.appendChild(th);
-		}
+		if (data && data.length > 0) {
+			const headerRow = table.insertRow(0);
 
-		// Заполнение таблицы данными
-		for (let i = 0; i < 5; i++) {
-			const row = table.insertRow(i + 1);
+			// Создание заголовков для таблицы
 			for (let j = 0; j < 10; j++) {
-				const cell = row.insertCell(j);
-				const key = Object.keys(data[i])[j];
-				cell.textContent = data[i][key]
+				const th = document.createElement('th');
+				const key = Object.keys(data[0])[j];
+				th.textContent = key ? key.charAt(0).toUpperCase() + key.slice(1) : '';
+				headerRow.appendChild(th);
 			}
 
-			addDeleteButton(row);  // Удаление каждой строки
+			// Заполнение таблицы данными
+			for (let i = 0; i < data.length; i++) {
+				const row = table.insertRow(i + 1);
+				for (let j = 0; j < 10; j++) {
+					const cell = row.insertCell(j);
+					const key = Object.keys(data[i])[j];
+					cell.textContent = key ? data[i][key] : '';
+				}
+
+				addDeleteButton(row);  // Удаление каждой строки
+			}
+		} else {
+			showPlaceholder('Данные не найдены');
 		}
 
 		// Очищаем контейнер и добавляем таблицу
@@ -103,7 +117,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		currentSortColumn = column;
 	}
 
-	// Добавление слушателя для таблицы
+	// Добавление слушателя событий для таблицы
 	function setupSortListener() {
 		const headerCells = tableContainer.querySelectorAll('th');
 		headerCells.forEach(cell => {
@@ -139,17 +153,34 @@ window.addEventListener('DOMContentLoaded', () => {
 		const table = tableContainer.querySelector('table');
 		const rowCount = table.rows.length;
 
-		if (index >= 0 && index < rowCount) {
-			table.deleteRow(index);
+		if (index === 0) {
+			table.innerHTML = '';
+
+			localStorage.removeItem('tableData');
+		} else if (index >= 0 && index < rowCount) {
+			const tableData = JSON.parse(localStorage.getItem('tableData')) || [];
+
+			if (index - 1 >= 0 && index - 1 < tableData.length) {
+				tableData.splice(index -1, 1);
+				localStorage.setItem('tableData', JSON.stringify(tableData));
+
+				renderTable(tableData);
+				setupSortListener();
+				setupDeleteButtons();
+			} else {
+				console.error('Последнюю строку удалить нельзя');
+				alert('Последнюю строку удалить нельзя');
+			}
 		} else {
 			console.error('Последнюю строку удалить нельзя');
-			alert('Последнюю строку удалить нельзя');
 		}
-
 	}
 
 	function clearTable() {
 		tableContainer.innerHTML = '<p>Данные еще не загруженны</p>';
+
+		// Очищение данных в localStorage при очистке таблицы
+		localStorage.removeItem('tableData');
 	}
 
 	function showPlaceholder(message) {
